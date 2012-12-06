a67baffed4db97b4c69590837ab4255e3b42b9b6
kit thahong <cthahong@gmail.com>
2012-12-05 20:49:52 -0500

    added if-else and specified time format to print

--- hello.rb
+++ hello.rb
@@ -1,1 +1,18 @@
-puts "hello"
+#!/usr/bin/env ruby
+# default name is Kit
+name = ARGV.first || "Kit"
+
+t1 = Time.now.hour
+t2 = Time.now
+
+if t1 < 18	
+	puts "Current Time : " + t2.strftime("%I:%M%p") 
+	puts "Have a nice day, #{name}!"
+
+elsif t1 > 18
+	puts "Current Time : " + t2.strftime("%I:%M%p") 
+	puts "Have a good night, #{name}!"
+	
+end
+
+
