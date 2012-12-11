fc1ed071b4c0568839d536810569e7fa4b467d44
kit thahong <cthahong@gmail.com>
2012-12-11 16:53:09 -0500

    added gets method

--- hello_again.rb
+++ hello_again.rb
@@ -1,1 +1,30 @@
-put "this is new file"
+#!/usr/bin/env ruby
+# default name is Kit
+name = ARGV.first || ""
+
+t1 = Time.now.hour
+t2 = Time.now
+
+if t1 < 18	
+	puts "Current Time : " + t2.strftime("%I:%M%p") 
+	if name == ""
+		puts "What is your name?"
+		val = gets
+		puts "Have a nice day, #{val}!"
+
+	else
+		puts "Have a nice day, #{name}!"
+	end
+
+elsif t1 > 18
+	puts "Current Time : " + t2.strftime("%I:%M%p") 
+	if name == ""
+		puts "Please tell me your name"
+		val = gets
+		"Have a good night, #{val}!"
+	else
+		puts "Have a good night, #{name}!"
+	end
+end
+
+
